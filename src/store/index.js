c10325b38970153adc0b4ad01f44c9cd172fd79b
21shunya <shabanchick@gmail.com>
2022-01-01 21:59:49 +0300

    add vuex module for modal

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import { auth } from './auth.module'
+import { context_menu } from './contextMenu.module'
 
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    auth
+    auth,
+    context_menu
   }
 });
